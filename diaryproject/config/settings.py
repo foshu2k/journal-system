8e68f7eef93bec589c085a68e90bfafadd2bd82f
foshu2k <foshu2k@gmail.com>
2026-05-19 13:04:15 +0800

    Update ALLOWED_HOSTS for railway

--- diaryproject/config/settings.py
+++ diaryproject/config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-fallback-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.railway.app']
 
 
 # Application definition
