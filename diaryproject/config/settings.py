bf4699b198ed8e51950864ab09a51a5ebe5dcaaf
foshu2k <foshu2k@gmail.com>
2026-05-19 13:31:27 +0800

    Update settings.py

--- diaryproject/config/settings.py
+++ diaryproject/config/settings.py
@@ -80,7 +80,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.getenv('DB_URL')
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
     )
 }
 
